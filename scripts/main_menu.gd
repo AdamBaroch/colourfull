71d10082499bec5c5b362265562cbc926d4d0cf1
AdamBaroch <baroch2006@gmail.com>
2025-04-29 17:44:14 +0200

    Final game for maturita

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,13 +3,14 @@ extends Control
 @onready var main_buttons: VBoxContainer = $Main_Buttons
 @onready var options: Panel = $Options
 @onready var credits: Button = $Credits
+@onready var credits_menu: Panel = $CREDITS_MENU
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	main_buttons.visible = true
 	options.visible = false
 	credits.visible = true
-
+	credits_menu.visible = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -23,6 +24,7 @@ func _on_options_pressed() -> void:
 	main_buttons.visible = false
 	options.visible = true
 	credits.visible = false
+	credits_menu.visible = false
 func _on_exit_game_pressed() -> void:
 	get_tree().quit()
 
@@ -31,3 +33,17 @@ func _on_options_back() -> void:
 	main_buttons.visible = true
 	options.visible = false
 	credits.visible = true
+	credits_menu.visible = false
+
+func _on_credits_menu_back() -> void:
+	main_buttons.visible = true
+	options.visible = false
+	credits.visible = true
+	credits_menu.visible = false
+
+
+func _on_credits_pressed() -> void:
+	main_buttons.visible = false
+	options.visible = false
+	credits.visible = false
+	credits_menu.visible = true
